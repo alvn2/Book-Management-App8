1c5842f4cf1a9e5c4f12a2ab5870a1fe9d4bacfc
victor kipkrui koech <kipkiruiv408@gmail.com>
2024-10-15 11:33:08 +0300

    registration logic

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book_club.db'
 db = SQLAlchemy(app)
+bcrypt =  Bcrypt(app)
+migrate = Migrate(app)
 
 from  server import models
 
--- backend/server/routes.py
+++ backend/server/routes.py
@@ -1,12 +1,25 @@
-from server import app
+from server import app, bcrypt, db
+from server.models import Users
+from flask import request, jsonify
 
 @app.route('/')
 def home():
     return '<h1>Home Page</h1>'
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
-    return '<h1>Register Page</h1>'
+    data = request.get_json()
+
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    user = Users(
+        username = data['username'],
+        email = data['email'],
+        image_file = data['image_file'],
+        password = hashed_password
+    )
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': f'Account created for {data["username"]}!'}), 201
 
 @app.route('/login')
 def login():
