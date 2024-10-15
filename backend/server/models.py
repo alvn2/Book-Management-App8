3c77ddaa758d0533007f5148c06d295bb2be1adf
victor kipkrui koech <kipkiruiv408@gmail.com>
2024-10-15 13:14:41 +0300

    tables update

--- backend/server/models.py
+++ backend/server/models.py
@@ -11,8 +11,29 @@ class Users(db.Model, UserMixin):
     username = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(50), nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.png')
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
     
+class Bookclub(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    owner = db.relationship('Users', backref='owner', lazy=True)
+    books = db.relationship('Book', backref='bookclub', lazy=True)
+
+    def __repr__(self):
+        return f"Bookclub('{self.name}', '{self.description}')"
+    
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_title = db.Column(db.String(50), nullable=False)
+    book_author = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    book_club_id = db.Column(db.Integer, db.ForeignKey('bookclub.id'), nullable=False)
+    book_club = db.relationship('Bookclub', backref='books', lazy=True)
+
+    def __repr__(self):
+        return f"Book('{self.book_title}', '{self.book_author}', '{self.description}')"
