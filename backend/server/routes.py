4d5c3e919e03859a1f0fcc6c39dd405aeb5b0a56
victor kipkrui koech <kipkiruiv408@gmail.com>
2024-10-15 12:47:31 +0300

    login funtionality

--- backend/server/routes.py
+++ backend/server/routes.py
@@ -10,6 +10,9 @@ def home():
 def register():
     data = request.get_json()
 
+    if Users.query.filter_by(email=data['email']).first():
+        return jsonify({'errors': {'email': 'Email already registered. please choose a different one'}}), 400
+
     hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
     user = Users(
         username = data['username'],
@@ -23,4 +26,12 @@ def register():
 
 @app.route('/login')
 def login():
-    return '<h1>Login Page</h1>'
+    data = request.get_json()
+
+    user = Users.query.filter_by(email=data['email']).first()
+    if user and bcrypt.check_password_hash(user.passwor,data['password']):
+        return jsonify({'message': f'user {user.username} found'})
+    else:
+        return jsonify({'message': f'Login failed, please check username or password'})
+    
+    
